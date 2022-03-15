0484fd70e624556c801cfb34918f4c10c1e35255
vukasinvujic <vukasinvujic021@gmail.com>
2022-03-15 18:12:42 +0100

    chaning class component to function component

--- src/pages/TodosPage.js
+++ src/pages/TodosPage.js
@@ -4,19 +4,9 @@ import { Link } from "react-router-dom";
 import { fetchTodos } from "../actions";
 import "../components/TodosPage.css";
 
-class TodosPage extends React.Component {
-  // createListTodos() {
-  //   return this.props.todos.map((todo) => {
-  //     return (
-  //       <li key={todo.id}>
-  //         {todo.name} {todo.description}
-  //       </li>
-  //     );
-  //   });
-  // }
-
-  giveTodos() {
-    return this.props.todos.map((todo) => {
+const TodosPage = (props) => {
+  const giveTodos = () => {
+    return props.todos.map((todo) => {
       return (
         <tr key={todo.id}>
           <td data-label> {todo.name} </td>
@@ -32,76 +22,34 @@ class TodosPage extends React.Component {
         </tr>
       );
     });
-  }
+  };
 
-  render() {
-    return (
-      <div className="todos-page-cont">
-        <h1>TodosPage</h1>
+  return (
+    <div className="todos-page-cont">
+      <h1>TodosPage</h1>
 
-        <br />
+      <br />
 
-        {/* <ul>{this.createListTodos()}</ul> */}
+      <table className="ui celled table table-class">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Description</th>
+            <th>Status</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>{giveTodos()}</tbody>
+      </table>
 
-        <table className="ui celled table table-class">
-          <thead>
-            <tr>
-              <th>Name</th>
-              <th>Description</th>
-              <th>Status</th>
-              <th>Actions</th>
-            </tr>
-          </thead>
-          <tbody>
-            {this.giveTodos()}
-            {/* <tr>
-              <td data-label="Name">James</td>
-              <td data-label="Description">24</td>
-              <td data-label="Status">Engineer</td>
-              <td data-label="Actions" className="action-button">
-                <Link to="/todos/edit/:id">
-                  <button className="ui green button">Edit</button>
-                </Link>
-                <button className="ui orange button">Done/Undo</button>
-                <button className="ui red button">Delete</button>
-              </td>
-            </tr>
-            <tr>
-              <td data-label="Name">Jill</td>
-              <td data-label="Description">26</td>
-              <td data-label="Status">Engineer</td>
-              <td data-label="Actions" className="action-button">
-                <Link to="/todos/edit/:id">
-                  <button className="ui green button">Edit</button>
-                </Link>
-                <button className="ui orange button">Done/Undo</button>
-                <button className="ui red button">Delete</button>
-              </td>
-            </tr>
-            <tr>
-              <td data-label="Name">Elyse</td>
-              <td data-label="Description">24</td>
-              <td data-label="Status">Designer</td>
-              <td data-label="Actions" className="action-button">
-                <Link to="/todos/edit/:id">
-                  <button className="ui green button">Edit</button>
-                </Link>
-                <button className="ui orange button">Done/Undo</button>
-                <button className="ui red button">Delete</button>
-              </td>
-            </tr> */}
-          </tbody>
-        </table>
-
-        <div className="button-cont">
-          <Link to="/todos/create">
-            <button className="ui primary button">Create Todo</button>
-          </Link>
-        </div>
+      <div className="button-cont">
+        <Link to="/todos/create">
+          <button className="ui primary button">Create Todo</button>
+        </Link>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 const mapStateToProps = (state) => {
   return { todos: state.todos };
